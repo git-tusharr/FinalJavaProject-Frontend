af20bd4a77c9255c350085f8b177fd11e2812087
Tushar Chourasiya <chourasiyatushar53@gmail.com>
2026-01-20 22:58:10 +0530

    check out

--- src/api/productApi.jsx
+++ src/api/productApi.jsx
@@ -1,3 +1,21 @@
+// import axios from "axios";
+
+// const API_BASE = "http://localhost:8080/api/products";
+
+// export const createProduct = async (productData) => {
+//   return axios.post(API_BASE, productData);
+// };
+
+
+
+
+
+
+// export const getProductBySlug = async (slug) => {
+//   return axios.get(`${API_BASE}/${slug}`);
+// };
+
+
 import axios from "axios";
 
 const API_BASE = "http://localhost:8080/api/products";
@@ -5,6 +23,11 @@ const API_BASE = "http://localhost:8080/api/products";
 export const createProduct = async (productData) => {
   return axios.post(API_BASE, productData);
 };
+
+
+
+
+
 
 export const getProductBySlug = async (slug) => {
   return axios.get(`${API_BASE}/${slug}`);
