f434b910e3dd9abb43ffc69035032d552d51b271
Tushar Chourasiya <chourasiyatushar53@gmail.com>
2026-01-22 23:25:55 +0530

    updates

--- src/api/productFeatureApi.jsx
+++ src/api/productFeatureApi.jsx
@@ -1,3 +1,17 @@
+// import axios from "axios";
+
+// const API_BASE = "http://localhost:8080/api/products";
+
+// export const saveProductFeatures = async (productId, features) => {
+//   // features = array of strings
+//   const res = await axios.post(`${API_BASE}/${productId}/features/bulk`, features);
+//   return res.data;
+// };
+
+// export const getProductFeatures = async (productId) => {
+//   const res = await axios.get(`${API_BASE}/${productId}/features`);
+//   return res.data;
+// };
 import axios from "axios";
 
 const API_BASE = "http://localhost:8080/api/products";
