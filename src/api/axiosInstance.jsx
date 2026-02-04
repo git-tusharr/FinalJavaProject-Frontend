c58008c7856369124b2a397de403bd65ac30db9a
Tushar Chourasiya <chourasiyatushar53@gmail.com>
2026-02-04 23:41:18 +0530

    api

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,4 +1,30 @@
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8080/auth",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// // attach token automatically
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
+
+
+
+
 import axios from "axios";
 
 const api = axios.create({
@@ -21,3 +47,7 @@ api.interceptors.request.use(
 );
 
 export default api;
+
+
+
+
